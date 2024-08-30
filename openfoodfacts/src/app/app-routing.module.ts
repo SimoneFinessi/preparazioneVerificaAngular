88a247d2544cfb26ba15306c9ba1a635b2d31e0d
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2024-08-30 09:38:42 +0000

    punto 2

--- openfoodfacts/src/app/app-routing.module.ts
+++ openfoodfacts/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
